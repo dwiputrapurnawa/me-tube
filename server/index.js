15739be76fc6fb928915f6a8bcff3a6511fca785
ericcornetto <putrapurnawa@gmail.com>
2020-06-10 14:47:02 +0800

    change port

--- server/index.js
+++ server/index.js
@@ -56,7 +56,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`)
