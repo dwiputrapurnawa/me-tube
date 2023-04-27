3f2753115cd8f43d3eada5bdf89c0a90ec90c425
dwiputrapurnawa <putrapurnawa@gmail.com>
2023-04-28 01:11:34 +0800

    fix database

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,1 @@
-// module.exports = { mongoURI: "mongodb+srv://dwiputrapurnawa:nevermind2000@cluster0.l0tap.mongodb.net/?retryWrites=true&w=majority" }
-
 module.exports = { mongoURI: "mongodb://localhost:27017/metube" }
